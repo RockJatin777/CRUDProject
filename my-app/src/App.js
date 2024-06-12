dd250eef2403fa10523d3d835b5bab8e6bce6dd1
RockJatin777 <rockjatin0748@gmail.com>
2024-06-12 22:15:00 +0530

    Update App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,4 +1,4 @@
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import {BrowserRouter, Routes, Route,} from 'react-router-dom'
 
 import RegisterPage from './Component/RegisterPage';
 import LoginPage from './Component/LoginPage'
